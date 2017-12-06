1826a21e2f4f786b5ee2f14446918cdc1d5c9b14
Piotr Delikat <piotrdellikat@gmail.com>
2017-12-06 23:59:58 +0100

    Socket connection for client server

---
+++ socket_connection/tcpClient.py
@@ -0,0 +1,18 @@
+import socket
+
+
+def main(message):
+    host = '127.0.0.1'
+    port = 5000
+
+    s = socket.socket()
+    s.connect((host, port))
+
+    s.send(message.encode('utf-8'))
+    data = s.recv(1024).decode('utf-8')
+    print("Received from server: " + data)
+    s.close()
+
+
+if __name__ == '__main__':
+        main()
